d2d321de0e73569f84d1d0bf931bca87e047679f
vinsis <6437295+vinsis@users.noreply.github.com>
2017-11-23 16:56:32 +0800

    added score mechanism to evolveV2

--- 2bevolve/evolveV2.js
+++ 2bevolve/evolveV2.js
@@ -4,7 +4,7 @@
 		var game = {};
 		game.handle = {};
 		var totalPairs = 2;
-	
+
 		game.type = [];
 		game.targetChars = [];
 		game.type[1] = 'l2r2';
@@ -14,26 +14,26 @@
 		game.type[3] = 'l2r1';
 		game.type[4] = 'l2r1';
 		game.targetChars[3] = '鼓';
-		game.targetChars[4] = '勵';			
-		
+		game.targetChars[4] = '勵';
+
 		/*game.width = 292;
 		game.height = 292;
-		
+
 		game.images = [];
 		game.collideImg = [];
 		game.loadedImages = 0;
 		game.reqdImages = 0;
-		
+
 		game.blockImages = [];
 		game.loadedBlockImages = 0;
 		game.reqdBlockImages = 0;
-		
+
 		game.mobileImageId = 'player';
 		game.mobileImagex = 2;
 		game.mobileImagey = 2;
 		game.mobileImageLastx = game.mobileImagex;
-		game.mobileImageLasty = game.mobileImagey;		
-		
+		game.mobileImageLasty = game.mobileImagey;
+
 		game.pathWidth = 45;
 		game.wallWidth = 3;
 		game.innerBlockWidth = 90;
@@ -41,10 +41,10 @@
 		game.outerWallY = game.outerWallLen + game.pathWidth - game.wallWidth;
 		//console.log(game.pathWidth+','+game.outerWallY);
 		game.passageWidth = 60;
-		
+
 		game.innerWallA = (2*game.pathWidth)+game.wallWidth;
 		game.innerWallB = game.innerWallA + game.innerBlockWidth + game.wallWidth;
-		
+
 		/*console.log('yo '+ game.innerWallA);
 		console.log(Math.floor(game.innerWallA + Math.random()*(game.innerWallB-game.innerWallA)));
 		console.log(Math.floor(game.innerWallA + Math.random()*(game.innerWallB-game.innerWallA)));
@@ -53,27 +53,27 @@
 		console.log('yo ' + game.innerWallB);
 		game.blockLocsX = [];
 		game.blockLocsY = [];
-		
+
 		game.keys = [];
 		game.speed = 3;
-		
+
 		game.bgContext = document.getElementById('bgCanvas').getContext('2d');
 		game.charContext = document.getElementById('charCanvas').getContext('2d');
-		
+
 		$(document).keydown(function(e){
 			game.keys[e.keyCode ? e.keyCode : e.which] = true;
 		});
 		$(document).keyup(function(e){
 			delete game.keys[e.keyCode ? e.keyCode : e.which];
 		});*/
-		
+
 		function init(){
 			i = game.collideImg.length;
 			//console.log(i);
 			var i1,j1,i2,j2;
-			
+
 			renderImage(game.blockImages[0],game.blockImages[0].x,game.blockImages[0].y,game.charContext);
-			
+
 			i1 = game.innerWallA;
 			j1 = game.innerWallA;
 			game.collideImg.push({
@@ -86,9 +86,9 @@
 			//console.log('inserted wall with coordinates - ' + game.collideImg[i].x1 + ', ' + game.collideImg[i].y1 + ', ' + game.collideImg[i].x2 + ', ' + game.collideImg[i].y2);
 			i++;
 			renderImage(game.images[0],game.innerWallA,game.innerWallA,game.bgContext);
-			
+
 			i1 = game.innerWallA;
-			j1 = game.innerWallA;			
+			j1 = game.innerWallA;
 			game.collideImg.push({
 				pic: game.images[1].pic,
 				x1 : game.innerWallA,
@@ -97,11 +97,11 @@
 				y2 : (j1 + game.images[1].h)
 			});
 			//console.log('inserted wall with coordinates - ' + game.collideImg[i].x1 + ', ' + game.collideImg[i].y1 + ', ' + game.collideImg[i].x2 + ', ' + game.collideImg[i].y2);
-			i++;			
+			i++;
 			renderImage(game.images[1],game.innerWallA,game.innerWallA,game.bgContext);
-			
+
 			i1 = game.innerWallA;
-			j1 = game.innerWallB;			
+			j1 = game.innerWallB;
 			game.collideImg.push({
 				pic: game.images[0].pic,
 				x1 : game.innerWallA,
@@ -112,9 +112,9 @@
 			//console.log('inserted wall with coordinates - ' + game.collideImg[i].x1 + ', ' + game.collideImg[i].y1 + ', ' + game.collideImg[i].x2 + ', ' + game.collideImg[i].y2);
 			i++;
 			renderImage(game.images[0],game.innerWallA,game.innerWallB,game.bgContext);
-			
+
 			i1 = game.innerWallB;
-			j1 = game.innerWallA;			
+			j1 = game.innerWallA;
 			game.collideImg.push({
 				pic: game.images[1].pic,
 				x1 : game.innerWallB,
@@ -125,14 +125,14 @@
 			//console.log('inserted wall with coordinates - ' + game.collideImg[i].x1 + ', ' + game.collideImg[i].y1 + ', ' + game.collideImg[i].x2 + ', ' + game.collideImg[i].y2);
 			i++;
 			renderImage(game.images[1],game.innerWallB,game.innerWallA,game.bgContext);
-			
+
 			randomY1 = Math.floor(game.pathWidth + Math.random()*(game.outerWallY-game.passageWidth-game.pathWidth));
 			//console.log('random1 is ' + randomY1);
 			randomY2 = Math.floor(game.pathWidth + Math.random()*(game.outerWallY-game.passageWidth-game.pathWidth));
 			//console.log('random2 is ' + randomY2);
-			
+
 			i1 = game.pathWidth;
-			j1 = game.pathWidth;			
+			j1 = game.pathWidth;
 			game.collideImg.push({
 				pic: game.images[2].pic,
 				x1 : game.pathWidth,
@@ -143,9 +143,9 @@
 			//console.log('inserted wall with coordinates - ' + game.collideImg[i].x1 + ', ' + game.collideImg[i].y1 + ', ' + game.collideImg[i].x2 + ', ' + game.collideImg[i].y2);
 			i++;
 			renderImage(game.images[2],game.pathWidth,game.pathWidth,game.bgContext);	//upper horizontal
-			
+
 			i1 = game.pathWidth;
-			j1 = game.pathWidth;						
+			j1 = game.pathWidth;
 			game.collideImg.push({	//upper half of the wall
 				pic: game.images[3].pic,
 				x1 : game.pathWidth,
@@ -168,9 +168,9 @@
 			renderImage(game.images[4],game.pathWidth,randomY1,game.bgContext);			//left passage
 			//renderImage(game.images[5],game.pathWidth,randomY1,game.bgContext);			//test image
 			//renderImage(game.images[5],game.pathWidth,(randomY1+game.passageWidth),game.bgContext);	//test image
-			
+
 			i1 = game.pathWidth;
-			j1 = game.outerWallY;			
+			j1 = game.outerWallY;
 			game.collideImg.push({
 				pic: game.images[2].pic,
 				x1 : game.pathWidth,
@@ -179,11 +179,11 @@
 				y2 : (j1 + game.images[2].h)
 			});
 			//console.log('inserted wall with coordinates - ' + game.collideImg[i].x1 + ', ' + game.collideImg[i].y1 + ', ' + game.collideImg[i].x2 + ', ' + game.collideImg[i].y2);
-			i++;			
+			i++;
 			renderImage(game.images[2],game.pathWidth,game.outerWallY,game.bgContext);	//lower horizontal
-			
+
 			i1 = game.outerWallY;
-			j1 = game.pathWidth;						
+			j1 = game.pathWidth;
 			game.collideImg.push({	//upper half of the wall
 				pic: game.images[3].pic,
 				x1 : game.outerWallY,
@@ -201,15 +201,15 @@
 				y2 : (j1 + game.images[3].h)
 			});
 			//console.log('inserted wall with coordinates - ' + game.collideImg[i].x1 + ', ' + game.collideImg[i].y1 + ', ' + game.collideImg[i].x2 + ', ' + game.collideImg[i].y2);
-			i++;			
+			i++;
 			renderImage(game.images[3],game.outerWallY,game.pathWidth,game.bgContext);	//right vertical
 			renderImage(game.images[4],game.outerWallY,randomY2,game.bgContext);		//right passage
 			//renderImage(game.images[5],game.outerWallY,randomY2,game.bgContext);			//test image
 			//renderImage(game.images[5],game.outerWallY,(randomY2+game.passageWidth),game.bgContext);	//test image
-			
+			game.playerTime = 10000;
 			loop();
 		}
-		
+
 		function loop(){
 			game.handle = requestAnimFrame(function() {
 				loop();
@@ -217,10 +217,20 @@
 			update();
 			//continue the game
 		}
-		
+
 		function update(){
+			$('#timer span').text(--game.playerTime);
+			if(game.playerTime < 0){
+				game.bgContext.font = "bold 20px monaco";
+				game.bgContext.fillStyle = "black";
+				game.bgContext.fillText('Time up!', game.width/2-50,game.height/2-50);
+				game.speed = 0;
+				cancelRequestAnimFrame(game.handle);
+				$('#scoreboard span').text($('#scoreboard span').text() + game.playerTime + '  ');
+				$('#totalscore span').text(parseInt($('#totalscore span').text()) + game.playerTime);
+			}
 			var i = getPlayerId(game.mobileImageId);
-			
+
 			if (game.keys[37] && game.blockImages[i].x > 0){//left arrow
 				game.mobileImageLastx = game.blockImages[i].x;
 				game.mobileImageLasty = game.blockImages[i].y;
@@ -228,7 +238,7 @@
 				//renderMobileImage(i,game.mobileImageLastx,game.mobileImageLasty,game.blockImages[i].width,game.blockImages[i].height);
 				updateCodeThatRepeatsOften(i,'left');
 			}
-			
+
 			if (game.keys[39] && game.blockImages[i].x < (game.width - game.blockImages[i].width)){//right arrow
 				game.mobileImageLastx = game.blockImages[i].x;
 				game.mobileImageLasty = game.blockImages[i].y;
@@ -236,7 +246,7 @@
 				//renderMobileImage(i,game.mobileImageLastx,game.mobileImageLasty,game.blockImages[i].width,game.blockImages[i].height);
 				updateCodeThatRepeatsOften(i,'right');
 			}
-			
+
 			if (game.keys[38] && game.blockImages[i].y > 0){//up arrow
 				game.mobileImageLastx = game.blockImages[i].x;
 				game.mobileImageLasty = game.blockImages[i].y;
@@ -244,7 +254,7 @@
 				//renderMobileImage(i,game.mobileImageLastx,game.mobileImageLasty,game.blockImages[i].width,game.blockImages[i].height);
 				updateCodeThatRepeatsOften(i,'up');
 			}
-			
+
 			if (game.keys[40] && game.blockImages[i].y < (game.height - game.blockImages[i].height)){//down arrow
 				game.mobileImageLastx = game.blockImages[i].x;
 				game.mobileImageLasty = game.blockImages[i].y;
@@ -253,10 +263,10 @@
 				updateCodeThatRepeatsOften(i,'down');
 			}
 		}
-		
+
 		function updateCodeThatRepeatsOften(i,direction){
 			//console.log('checking for collision with walls ' + game.collideImg.length);
-			
+
 			for(n in game.blockImages){
 				if(i!=n && game.blockImages[n].showOnStart == true){
 					if (detectCollision(game.blockImages[i],game.blockImages[n],direction)){
@@ -274,7 +284,7 @@
 					}
 				}
 			}
-			
+
 			if(game.blockImages[i].id != 'player'){
 				for(n in game.collideImg)
 				{
@@ -290,7 +300,7 @@
 			//}
 			//reDrawCanvas();
 		}
-		
+
 		function detectCollisionWithWalls(mobileImage, staticImage, direction){
 			sy1 = staticImage.y1;
 			sy2 = staticImage.y2;
@@ -303,7 +313,7 @@
 			mx1 = mobileImage.x;
 			mx2 = mobileImage.x + mobileImage.width;
 			//mxmid = mobileImage.x + (mobileImage.width/2);
-			
+
 			if(direction == 'right'){
 				if((my2<=sy2 && my2>=sy1) || (my1<=sy2 && my1>=sy1) || (my1<=sy1 && my2>=sy1)){
 					if(mx2<=sx2 && mx2>=sx1){
@@ -343,10 +353,10 @@
 						return true;
 					}
 				}
-			}			
+			}
 			return false;
 		}
-		
+
 		function detectCollision(mobileImage, staticImage, direction){
 			sy1 = staticImage.y;
 			sy2 = staticImage.y + staticImage.height;
@@ -358,7 +368,7 @@
 			mx1 = mobileImage.x;
 			mx2 = mobileImage.x + mobileImage.width;
 			mxmid = mobileImage.x + (mobileImage.width/2);
-			
+
 			if (mobileImage.id == 'player'){
 					if(((mx1>=sx1 && mx1<=sx2) || (mx2>=sx1 && mx2<=sx2)) && ((my1>=sy1 && my1<=sy2) || (my2>=sy1 && my2<=sy2))){
 					game.blockImages.splice(getPlayerId(game.mobileImageId),1);
@@ -395,7 +405,7 @@
 								return false;
 							}
 						}
-					}					
+					}
 				} else
 				if(direction == 'down'){
 					if((mx2<=sx2 && mx2>=sx1) || (mx1<=sx2 && mx1>=sx1) || (mxmid<=sx2 && mxmid>=sx1)){
@@ -425,11 +435,11 @@
 								return false;
 							}
 						}
-					}					
+					}
 				}
 			}
 		}
-		
+
 		function checkIfGameComplete(mobileImage,staticImage){
 			if(mobileImage.parent == 'none' && staticImage.parent == 'none'){
 				//updateInstructions('Congratulations, you finished the game successfully!');
@@ -438,9 +448,11 @@
 				game.bgContext.fillText('Game Over!', game.width/2-50,game.height/2-50);
 				game.speed = 0;
 				cancelRequestAnimFrame(game.handle);
+				$('#scoreboard span').text($('#scoreboard span').text() + game.playerTime + '  ');
+				$('#totalscore span').text(parseInt($('#totalscore span').text()) + game.playerTime);
 			}
 		}
-		
+
 		function fitToMerge(movingInDirection,canCombineInDirection,mobileImageParent,staticImageParent){
 			if(movingInDirection == canCombineInDirection && mobileImageParent == staticImageParent){
 				return true;
@@ -451,7 +463,7 @@
 			game.charContext.clearRect(oldPosx,oldPosy,imageWidth,imageHeight);
 			game.charContext.drawImage(game.blockImages[mobileImageId].pic,game.blockImages[mobileImageId].x,game.blockImages[mobileImageId].y);
 		}
-		
+
 		function reDrawCanvas(){
 			game.charContext.clearRect(0,0,game.width,game.height);
 			//console.log(game.blockImages.length);
@@ -461,18 +473,18 @@
 				}
 			}
 		}
-		
+
 		function getPlayerId(idName){
 			for(i in game.blockImages){
 				if(game.blockImages[i].id == idName){ return i; }
 			}
 		}
-		
+
 		function renderImage(image, xLoc, yLoc,context){
 			//console.log('rendering at ' + xLoc + ' ' + yLoc);
 			context.drawImage(image.pic, xLoc, yLoc);
 		}
-		
+
 		function loadImageFromArr(imageArr){
 			game.reqdImages = imageArr.length;
 			for(i in imageArr){
@@ -492,7 +504,7 @@
 				};
 			}
 		}
-		
+
 		function verifyUploadOfAllImages(int1,type1,int2,type2){
 			//console.log('comparing ' + game.loadedImages + ' with ' + game.reqdImages);
 			if(game.loadedImages >= game.reqdImages){
@@ -510,16 +522,16 @@
 				},1);
 			}
 		}
-		
+
 		function resetImageParams(){
 			for(i in game.images){
 				game.images[i].w = game.images[i].pic.width;
 				game.images[i].h = game.images[i].pic.height;
 				//console.log('updated an image with parameters' + i + ',' + game.images[i].w + ',' + game.images[i].h);
 			}
-		
+
 		}
-		
+
 		function shuffleArraysInSync(array1,array2) {
 			if(array1.length == array2.length){
 				for (var i = array1.length - 1; i > 0; i--) {
@@ -537,7 +549,7 @@
 				return false;
 			}
 		}
-		
+
 		function createImageNames(charSNo,charType,isFirst){
 			if(isFirst){
 				game.blockLocsX.length=0;
@@ -546,7 +558,7 @@
 				game.blockLocsX.push(posA1);
 				posA2 = game.innerWallA-game.pathWidth+2;
 				game.blockLocsY.push(posA2);
-				posB1 = Math.floor(game.pathWidth + Math.random()*(game.outerWallY-game.pathWidth-40)); 
+				posB1 = Math.floor(game.pathWidth + Math.random()*(game.outerWallY-game.pathWidth-40));
 				game.blockLocsX.push(posB1);
 				posB2 = game.pathWidth-game.pathWidth+2;
 				game.blockLocsY.push(posB2);
@@ -563,7 +575,7 @@
 			else{
 				game.blockLocsX.length=0;
 				game.blockLocsY.length=0;
-				posA1 = Math.floor(game.innerWallA + Math.random()*(game.innerWallB-game.innerWallA-40)); 
+				posA1 = Math.floor(game.innerWallA + Math.random()*(game.innerWallB-game.innerWallA-40));
 				game.blockLocsX.push(posA1);
 				posA2 = game.innerWallB+5;
 				game.blockLocsY.push(posA2);
@@ -583,7 +595,7 @@
 			}
 			if(charType.indexOf('l2') >= 0){
 				loadImage(charSNo+'lt',true,charSNo+'l','down',game.blockLocsX[0],game.blockLocsY[0]);
-				loadImage(charSNo+'lb',true,charSNo+'l','up',game.blockLocsX[1],game.blockLocsY[1]);		
+				loadImage(charSNo+'lb',true,charSNo+'l','up',game.blockLocsX[1],game.blockLocsY[1]);
 				loadImage(charSNo+'l',false,charSNo+charType,'right',0,0);
 			} else
 			if(charType.indexOf('l1') >= 0){
@@ -615,7 +627,7 @@
 			}
 			loadImage(charSNo+charType,false,'none','none',0,0);
 		}
-		
+
 		function loadImage(imageName,displayOnStart,parentName,combineWhenDirectionIs,xLoc,yLoc){
 			//console.log(imageName + ' is gonna load at ' + xLoc + ',' + yLoc);
 			var image = new Image;
@@ -637,15 +649,15 @@
 				parent: parentName,
 				direction: combineWhenDirectionIs
 			});
-			
+
 			game.blockImages[i].pic.onload = function()
 			{
 				game.blockImages[i].width = image.width;
 				game.blockImages[i].height = image.height;
-				game.loadedBlockImages++; 
+				game.loadedBlockImages++;
 			}
 		}
-		
+
 		function checkBlockImagesUpload(){
 			if(game.reqdBlockImages <= game.loadedBlockImages){
 				init();
@@ -655,26 +667,26 @@
 					{ checkBlockImagesUpload(); },1);
 			}
 		}
-		
+
 		function letsGetRolling(int1, type1, int2, type2){
 			game.width = 292;
 			game.height = 292;
-			
+
 			game.images = [];
 			game.collideImg = [];
 			game.loadedImages = 0;
 			game.reqdImages = 0;
-			
+
 			game.blockImages = [];
 			game.loadedBlockImages = 0;
 			game.reqdBlockImages = 0;
-			
+
 			game.mobileImageId = 'player';
 			game.mobileImagex = 2;
 			game.mobileImagey = 2;
 			game.mobileImageLastx = game.mobileImagex;
-			game.mobileImageLasty = game.mobileImagey;		
-			
+			game.mobileImageLasty = game.mobileImagey;
+
 			game.pathWidth = 45;
 			game.wallWidth = 3;
 			game.innerBlockWidth = 90;
@@ -682,25 +694,25 @@
 			game.outerWallY = game.outerWallLen + game.pathWidth - game.wallWidth;
 			//console.log(game.pathWidth+','+game.outerWallY);
 			game.passageWidth = 60;
-			
+
 			game.innerWallA = (2*game.pathWidth)+game.wallWidth;
 			game.innerWallB = game.innerWallA + game.innerBlockWidth + game.wallWidth;
-			
+
 			game.blockLocsX = [];
 			game.blockLocsY = [];
-			
+
 			game.keys = [];
 			game.speed = 3;
-			
+
 			game.bgContext = document.getElementById('bgCanvas').getContext('2d');
 			game.charContext = document.getElementById('charCanvas').getContext('2d');
-		
+
 			$(document).keydown(function(e){
 				game.keys[e.keyCode ? e.keyCode : e.which] = true;
 			});
 			$(document).keyup(function(e){
 				delete game.keys[e.keyCode ? e.keyCode : e.which];
-			});		
+			});
 			imageNamesArr = [
 			'innerwallH',		//0
 			'innerwallV',		//1
@@ -714,8 +726,8 @@
 			$('#tb2').text(game.targetChars[int2]);
 			verifyUploadOfAllImages(int1, type1, int2, type2);
 		}
-		
-		
+
+
 		$('#newGame').click(function(){
 			var rand = Math.floor(Math.random()*totalPairs);
 			rand *= 2;
